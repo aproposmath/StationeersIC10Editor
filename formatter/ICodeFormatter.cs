8926cab54fc18f10bc948e4a0df446a60d83c3e8
aproposmath <aproposmath@proton.me>
2026-01-02 13:09:35 +0100

    cleanup

--- formatter/ICodeFormatter.cs
+++ formatter/ICodeFormatter.cs
@@ -172,7 +172,7 @@ public abstract class ICodeFormatter
     {
         var lines = code.Split('\n');
         Lines.Clear();
-        L.Debug($"Resetting code with {lines.Length} lines");
+        // L.Debug($"Resetting code with {lines.Length} lines");
         foreach (var line in lines)
             AppendLine(line);
         OnCodeChanged();
